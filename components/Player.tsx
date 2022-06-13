bfd56cd7259ff7234bc3a5b98516169c3cb1a9d8
binoy638 <binoy638@gmail.com>
2022-06-13 22:03:06 +0530

    feat: saved video progress in localstorage

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,7 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
+import { VideoJsPlayer } from 'video.js';
 
 import { IVideo } from '../@types';
 import { getPreviewLink, getVideoLink } from '../API';
+import { useTypedDispatch } from '../hooks/useTypedDispatch';
+import { setProgress } from '../store/slice/player.slice';
 import { VideoPlayerEvents } from '../utils/videoPlayerEvents';
 import VideoJS from '../VideoJs/VideoJs';
 
@@ -10,6 +13,8 @@ interface PlayerProps {
 }
 
 const Player: FC<PlayerProps> = ({ video }) => {
+  const dispatch = useTypedDispatch();
+
   const options = {
     sources: [
       {
@@ -18,11 +23,32 @@ const Player: FC<PlayerProps> = ({ video }) => {
     ]
   };
 
+  const onReady = useCallback((player: videojs.VideoJsPlayer) => {
+    const savedVolume = localStorage.getItem('volume');
+    if (savedVolume && (savedVolume === '0' || Number(savedVolume))) {
+      player.volume(Number(savedVolume));
+    }
+
+    const currentTime = localStorage.getItem(`${video.slug}-currentTime`);
+    if (currentTime) {
+      player.currentTime(Number(currentTime));
+    }
+  }, []);
+
+  const onTimeUpdate = useCallback((player: VideoJsPlayer) => {
+    const currentTime = Math.round(player.currentTime());
+    if (currentTime % 5 === 0) {
+      const key = `${video.slug}-currentTime`;
+      localStorage.setItem(key, currentTime.toString());
+    }
+  }, []);
+
   return (
     <VideoJS
-      onReady={VideoPlayerEvents.onReady}
+      onReady={onReady}
       options={options}
       subtitles={video.subtitles}
+      onTimeUpdate={onTimeUpdate}
       onVolumeChange={VideoPlayerEvents.onVolumeChange}
       progressBarPreviewUrl={
         video.progressPreview
--- utils/videoPlayerEvents.ts
+++ utils/videoPlayerEvents.ts
@@ -1,12 +1,5 @@
 /* eslint-disable unicorn/no-static-only-class */
 export class VideoPlayerEvents {
-  static onReady = (player: videojs.VideoJsPlayer) => {
-    const savedVolume = localStorage.getItem('volume');
-    if (savedVolume && (savedVolume === '0' || Number(savedVolume))) {
-      player.volume(Number(savedVolume));
-    }
-  };
-
   static onVolumeChange = (player: videojs.VideoJsPlayer) => {
     localStorage.setItem('volume', player.volume().toString());
   };
