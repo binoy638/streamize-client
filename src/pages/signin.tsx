c34e4519493d543f14cc4fcdef10e1263e60661c
binoy638 <binoy638@gmail.com>
2022-08-15 12:07:38 +0530

    feat: added guest login details in login page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,4 +1,19 @@
-import { Button, PasswordInput, TextInput } from '@mantine/core';
+import {
+  CheckIcon,
+  ClipboardIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/outline';
+import {
+  ActionIcon,
+  Button,
+  CopyButton,
+  Paper,
+  PasswordInput,
+  Popover,
+  Text,
+  TextInput,
+  Tooltip,
+} from '@mantine/core';
 import { useMutation } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 import Head from 'next/head';
@@ -6,6 +21,54 @@ import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 import { signIn } from '../API';
+
+const CopyBtn = ({ value }: { value: string }) => {
+  return (
+    <CopyButton value={value} timeout={2000}>
+      {({ copied, copy }) => (
+        <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow position="right">
+          <ActionIcon color={copied ? 'teal' : 'gray'} onClick={copy}>
+            {copied ? (
+              <CheckIcon className="h-4 w-4" />
+            ) : (
+              <ClipboardIcon className="h-4 w-4" />
+            )}
+          </ActionIcon>
+        </Tooltip>
+      )}
+    </CopyButton>
+  );
+};
+
+const GuestUserLoginDetails = () => {
+  const [opened, setOpened] = useState(false);
+  return (
+    <Popover
+      width={200}
+      position="bottom"
+      opened={opened}
+      onChange={setOpened}
+      shadow="md"
+    >
+      <Popover.Target>
+        <InformationCircleIcon
+          className="h-5 w-5 cursor-pointer"
+          onClick={() => setOpened((o) => !o)}
+        />
+      </Popover.Target>
+      <Popover.Dropdown>
+        <div className="flex justify-between">
+          <Text size="sm">Username: guest</Text>
+          <CopyBtn value="guest" />
+        </div>
+        <div className="flex justify-between">
+          <Text size="sm">Password: password</Text>
+          <CopyBtn value="password" />
+        </div>
+      </Popover.Dropdown>
+    </Popover>
+  );
+};
 
 export default function SignIn() {
   const [username, setUsername] = useState('');
@@ -33,30 +96,46 @@ export default function SignIn() {
   };
 
   return (
-    <div className=" flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center ">
       <Head>
         <title>Streamize - Sign in</title>
       </Head>
-      <form onSubmit={handleSubmit} className="w-96">
-        <TextInput
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          label="Username"
-          required
-        />
-        <PasswordInput
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          label="Password"
-          required
-        />
-        {error && <p className="mt-4 w-full text-sm text-red-500">{error}</p>}
-        <Button color="red" type="submit" className="mt-4">
-          Login
-        </Button>
-      </form>
+
+      <div className="flex justify-center">
+        <Paper withBorder sx={{ position: 'relative' }}>
+          <div className="absolute right-0 p-4">
+            <GuestUserLoginDetails />
+          </div>
+          <span className="flex justify-center p-4 font-teko text-2xl  text-primary  ">
+            Streamize
+          </span>
+          <form
+            onSubmit={handleSubmit}
+            className="flex w-96 flex-col gap-2 py-10 px-6"
+          >
+            <TextInput
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              label="Username"
+              required
+            />
+            <PasswordInput
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              label="Password"
+              required
+            />
+            {error && (
+              <p className="mt-4 w-full text-sm text-red-500">{error}</p>
+            )}
+            <Button color="red" mt={20} type="submit">
+              Login
+            </Button>
+          </form>
+        </Paper>
+      </div>
     </div>
   );
 }
